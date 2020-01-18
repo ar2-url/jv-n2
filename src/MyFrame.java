4ff860e6375e004de99723e6fe3cecfed606fc16
Artur Knul <artur.knul@gmail.com>
2020-01-18 11:33:28 +0000

    Add F2

---
+++ src/MyFrame.java
@@ -0,0 +1,13 @@
+
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+
+    public MyFrame() {
+        super("Javalon3group frame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
+
